547a3af928bbf7cbc350e8b9eafebd2c2c8bfc03
Anna Kapanadze <anna.kapanadze@outlook.com>
2026-03-01 19:23:06 +0000

    Update read_my_library.py

--- read_my_library.py
+++ read_my_library.py
@@ -5,8 +5,6 @@ def show_my_books(goodreads_library_export.csv):
     with open(goodreads_library_export.csv, mode='r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            # This neatly prints each book from your list
             print(f"[{row['Status']}] {row['Title']} by {row['Author']} - ⭐ {row['Rating']}/5")
 
-# Run the function for your non-fiction list
 show_my_books('non_fiction_list.csv')
