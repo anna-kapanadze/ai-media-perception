5c3bd2dbff4039f9364d9422f21e63341e9a60db
Anna Kapanadze <anna.kapanadze@outlook.com>
2026-03-02 22:22:59 +0000

    Update library_stats.py

--- library_stats.py
+++ library_stats.py
@@ -5,9 +5,9 @@ ratings = []
 with open('goodreads_library_export.csv', mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
-        # Convert the rating from a string to a number (integer)
+    
         rating = int(row['My Rating'])
-        if rating > 0: # Ignore books not yet rated 
+        if rating > 0: # Ignorinb the books not yet rated 
             ratings.append(rating)
 
 if ratings:
